a739dc388bd682edcc2b4395840265f1ea80de68
ricardo1181 <ricardo39985@gmail.com>
2022-10-02 14:15:47 -0400

    Require mongoose/ emport model

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,8 @@
+const mongoose = require('mongoose');
 const flightSchema = new mongoose.Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
   airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
   flightNo: { type: Number, min: 10, max: 9999 },
   departs: { type: Date, default: new Date().getFullYear() + 1 },
 });
+module.exports = mongoose.model('Flight', flightSchema)

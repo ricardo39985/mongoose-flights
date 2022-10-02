9a57972c58aa1aceb002d4cc32aa451f3ce2962a
ricardo1181 <ricardo39985@gmail.com>
2022-10-02 13:59:10 -0400

    Add controller funtions

---
+++ controllers/flights.js
@@ -0,0 +1,16 @@
+module.exports = {
+  index,
+  create,
+  delete: deleteFlight,
+  show,
+  new: newFlight,
+};
+
+function index(req, res) {}
+function newFlight(req, res) {}
+
+function deleteFlight(req, res) {}
+
+function show(req, res) {}
+function update(req, res) {}
+function create(req, res) {}

821714699982ab5d7da8af39d967fa74c4881cfe
ricardo1181 <ricardo39985@gmail.com>
2022-10-02 13:59:25 -0400

    Add routes

--- routes/flights.js
+++ routes/flights.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', flightsCtrl.index);
+router.get('/new', flightsCtrl.new);
+router.get('/:id', flightsCtrl.show);
+router.get('/:id/edit', flightsCtrl.index);
+router.post('/', flightsCtrl.create);
+router.delete('/:id', flightsCtrl.index);
+
+
+
+
 
 module.exports = router;

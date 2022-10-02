eae5b7c1d044db0bf0ae1c7f6c1bd19593ccf71d
ricardo1181 <ricardo39985@gmail.com>
2022-10-02 13:50:41 -0400

    create flight schema

---
+++ models/flight.js
@@ -0,0 +1,6 @@
+const flightSchema = new mongoose.Schema({
+  airline: { type: String, enum: ['American', 'Southwest', 'United'] },
+  airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
+  flightNo: { type: Number, min: 10, max: 9999 },
+  departs: { type: Date, default: new Date().getFullYear() + 1 },
+});

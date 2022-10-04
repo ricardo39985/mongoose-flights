8e1e2db5aa1891ab79cd2d632b9ca175e2530111
ricardo1181 <ricardo39985@gmail.com>
2022-10-04 10:34:27 -0400

    Add defaults for year

--- models/flight.js
+++ models/flight.js
@@ -4,6 +4,6 @@ const flightSchema = new Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
   airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
   flightNo: { type: Number, min: 10, max: 9999 },
-  departs: { type: Date, default: new Date().getFullYear() + 1 },
+  departs: { type: Date, default: new Date(new Date().setFullYear(new Date().getFullYear() + 1)) },
 });
 module.exports = mongoose.model('Flight', flightSchema);

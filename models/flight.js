6456b5e9ae9ee9a6d72c48440db385bd29a48f6f
ricardo1181 <ricardo39985@gmail.com>
2022-10-04 10:32:27 -0400

    Fix typo

--- models/flight.js
+++ models/flight.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-const flightSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+const flightSchema = new Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
   airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
   flightNo: { type: Number, min: 10, max: 9999 },
   departs: { type: Date, default: new Date().getFullYear() + 1 },
 });
-module.exports = mongoose.model('Flight', flightSchema)
+module.exports = mongoose.model('Flight', flightSchema);

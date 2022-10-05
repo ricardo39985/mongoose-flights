3a215097145059d05c500cea807bd99d3f05af78
ricardo1181 <ricardo39985@gmail.com>
2022-10-04 22:43:07 -0400

    Add reference to destination

--- models/flight.js
+++ models/flight.js
@@ -5,5 +5,6 @@ const flightSchema = new Schema({
   airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
   flightNo: { type: Number, min: 10, max: 9999 },
   departs: { type: Date, default: new Date(new Date().setFullYear(new Date().getFullYear() + 1)) },
+  destinations: [{ type: Schema.Types.ObjectId, ref: 'Destination' }],
 });
 module.exports = mongoose.model('Flight', flightSchema);

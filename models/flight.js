3363307e120cbb8ad83a85da624e6070c5e90f05
ricardo1181 <ricardo39985@gmail.com>
2022-10-07 13:18:32 -0400

    Require Destination model. Update show to use destination model

--- models/flight.js
+++ models/flight.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const flightSchema = new Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
-  airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
+  // airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
   flightNo: { type: Number, min: 10, max: 9999 },
   departs: { type: Date, default: new Date(new Date().setFullYear(new Date().getFullYear() + 1)) },
   destinations: [{ type: Schema.Types.ObjectId, ref: 'Destination' }],

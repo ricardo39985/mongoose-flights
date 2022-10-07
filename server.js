b4022597be42f0a9350055811b274570afef6001
ricardo1181 <ricardo39985@gmail.com>
2022-10-07 13:20:11 -0400

    Require and use destinations router

--- server.js
+++ server.js
@@ -6,41 +6,39 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
-const methodOverride = require('method-override')
+const destinationsRouter = require('./routes/destinations');
+const methodOverride = require('method-override');
 
 var app = express();
 // Start Mongo Connection
-require('./config/databse')
+require('./config/databse');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use(express.static('public'))
-
-
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use(express.static('public'));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(methodOverride('_method'))
-
-
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
+app.use('/', destinationsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

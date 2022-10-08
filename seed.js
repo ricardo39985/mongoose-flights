e70853b5b97346a8ac59785b24e95791ad09f97d
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 19:13:56 -0400

    Delete tickets

--- seed.js
+++ seed.js
@@ -4,12 +4,12 @@ const Flight = require('./models/flight');
 const Ticket = require('./models/ticket');
 
 const p1 = Flight.deleteMany({});
-// const p2 = Ticket.deleteMany({});
+const p2 = Ticket.deleteMany({});
 
 
 async function seedAsync() {
   try {
-    await Promise.all([p1]);
+    await Promise.all([p1,p2]);
   } catch (err) {
   }
   process.exit();

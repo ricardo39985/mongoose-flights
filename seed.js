941fd4e768472fecf60a149b13f6d07a36346742
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 15:04:25 -0400

    require ticket model

--- seed.js
+++ seed.js
@@ -1,8 +1,11 @@
 require('./config/databse');
 
 const Flight = require('./models/flight');
+const Ticket = require('./models/ticket');
 
 const p1 = Flight.deleteMany({});
+// const p2 = Ticket.deleteMany({});
+
 
 async function seedAsync() {
   try {

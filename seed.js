4be5d052193371968885774c7a274c9c4483635d
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 14:04:58 -0400

    Remove some code

--- seed.js
+++ seed.js
@@ -1,14 +1,13 @@
 require('./config/databse');
 
-const Destination = require('./models/destination');
 const Flight = require('./models/flight');
 
 const p1 = Flight.deleteMany({});
+
 async function seedAsync() {
   try {
     await Promise.all([p1]);
   } catch (err) {
-    console.log(err);
   }
   process.exit();
 }

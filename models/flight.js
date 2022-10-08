34e48a3de7e9a0301864765efdb1c7d66c3807b4
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 14:03:34 -0400

    Update model

--- models/flight.js
+++ models/flight.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const destinationSchema = new Schema({
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+  },
+  arrival: Date,
+});
 const flightSchema = new Schema({
   airline: { type: String, enum: ['American', 'Southwest', 'United'] },
-  // airport: { type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN' },
   flightNo: { type: Number, min: 10, max: 9999 },
   departs: { type: Date, default: new Date(new Date().setFullYear(new Date().getFullYear() + 1)) },
-  destinations: [{ type: Schema.Types.ObjectId, ref: 'Destination' }],
+  destinations: [destinationSchema],
 });
 module.exports = mongoose.model('Flight', flightSchema);

06cda163bd42101fc62a2a92c5179853421aa385
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 19:13:01 -0400

    Add delete route

--- routes/tickets.js
+++ routes/tickets.js
@@ -4,4 +4,5 @@ const ticketsCtrl = require('../controllers/tickets');
 
 router.get('/flights/:flightId/tickets/new', ticketsCtrl.new);
 router.post('/flights/:flightId/tickets', ticketsCtrl.create);
+router.delete('/tickets/:id', ticketsCtrl.delete)
 module.exports=router

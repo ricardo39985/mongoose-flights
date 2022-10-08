cbcef70a81ec26a469d96f1e22e56a70779a5953
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 19:11:55 -0400

    Implement create and delete controller functionality

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,11 +1,26 @@
+const Flight = require('../models/flight');
+const Ticket = require('../models/ticket');
 module.exports = {
   new: newTicket,
   create,
+  delete: deleteOne,
 };
 function newTicket(req, res) {
-  res.render('tickets/new');
+  Flight.findById(req.params.flightId, function (err, flight) {
+    res.render('tickets/new', { flight });
+  });
 }
 function create(req, res) {
-  console.log(req.body);
-  res.redirect(`/flights/${req.params.id}/tickets/new`);
+  Flight.findById(req.params.flightId, function (err, flight) {
+    const newTicket = new Ticket(req.body);
+    newTicket.flight = flight;
+    newTicket.save();
+    res.redirect(`/flights/${req.params.flightId}`);
+  });
+}
+
+function deleteOne(req, res) {
+  Ticket.deleteOne({ _id: req.params.id }, function (err, ticket) {
+    res.redirect(`/flights/${req.body.flightId}`);
+  });
 }

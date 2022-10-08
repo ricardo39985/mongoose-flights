96e198a0cbea49b3f805a5fb8dee98247b805806
ricardo1181 <ricardo39985@gmail.com>
2022-10-08 19:10:06 -0400

    Format for spacing

--- controllers/destinations.js
+++ controllers/destinations.js
@@ -6,7 +6,6 @@ function create(req, res) {
   Flight.findById(req.params.id, function (err, flight) {
     flight.destinations.push(req.body);
     flight.save(function (err) {
-      console.log(flight)
       res.redirect(`/flights/${flight._id}`);
     });
   });
